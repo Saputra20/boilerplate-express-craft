c9af5ccd13daec7f25e7d06468f1d24126985043
Saputra Nugraha <saputra.nugraha46@gmail.com>
2025-05-18 07:51:18 +0700

    feat: delete generator

--- src/@core/generator/templates/service.js
+++
@@ -1,6 +0,0 @@
-const BaseService = require('../../../../@core/service/BaseService');
-const { __Name__ } = require('../../../../database/models');
-
-class __Name__Service extends BaseService {}
-
-module.exports = new __Name__Service(__Name__);
--- src/@core/generator/templates/validation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const createOrUpdate = {
-  body: Joi.object().keys({ __joi_fields__ }),
-};
-
-module.exports = {
-  createOrUpdate,
-};
